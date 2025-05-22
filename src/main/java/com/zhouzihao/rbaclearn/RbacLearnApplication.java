be95d656f86eadbb4f3c90a8258b1ce2eb41e69c
sAkuraOfficial <mchaohao159@outlook.com>
2025-05-22 23:57:57 +0800

    Initial commit

---
+++ src/main/java/com/zhouzihao/rbaclearn/RbacLearnApplication.java
@@ -0,0 +1,13 @@
+package com.zhouzihao.rbaclearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhouzihao/rbaclearn/RbacLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouzihao.rbaclearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
